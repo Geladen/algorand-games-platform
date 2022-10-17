2a384e9e8ae5d4b9f19febdd607090f21561c871
Roberto Pettinau <roberto.pettinau99@gmail.com>
2022-10-17 15:18:17 +0200

    Disable create_platform

--- src/algogames/main.py
+++ src/algogames/main.py
@@ -4,6 +4,6 @@ load_dotenv(dotenv_path='ops/env/platform.env')
 
 from game_platform.interact_platform import _create_platform, interact_platform
 
-_create_platform()
+# _create_platform()
 # interact_swap()
 interact_platform()
